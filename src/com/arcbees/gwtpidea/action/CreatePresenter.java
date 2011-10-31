2d08eb25e19f9a481870a330611e4318d634423d
Javier Fernandez-Ivern <javier@ivern.org>
2011-10-31 16:29:40 -0400

    Initial import, created menu structure for file creation actions

---
+++ src/com/arcbees/gwtpidea/action/CreatePresenter.java
@@ -0,0 +1,12 @@
+package com.arcbees.gwtpidea.action;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class CreatePresenter extends AnAction {
+
+  public void actionPerformed(AnActionEvent e) {
+    // TODO: insert action logic here
+  }
+
+}
