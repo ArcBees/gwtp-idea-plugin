dd83674260acf0089efe83123e3f9758e1eccd01
branflake2267@gmail.com <branflake2267@gmail.com>
2013-09-19 18:14:00 -0700

    Created Create Presenter Action.

---
+++ src/com/arcbees/plugin/idea/wizards/createpresenter/CreatePresenterAction.java
@@ -0,0 +1,10 @@
+package com.arcbees.plugin.idea.wizards.createpresenter;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class CreatePresenterAction extends AnAction {
+  public void actionPerformed(AnActionEvent e) {
+    // TODO: insert action logic here
+  }
+}
