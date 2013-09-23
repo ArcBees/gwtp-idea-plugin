6a653be499a63f58e210f939074d41c496d94ac4
branflake2267@gmail.com <branflake2267@gmail.com>
2013-09-22 22:50:54 -0700

    Working on create presenter form.

--- src/com/arcbees/plugin/idea/wizards/createpresenter/CreatePresenterForm.java
+++ src/com/arcbees/plugin/idea/wizards/createpresenter/CreatePresenterForm.java
@@ -9,6 +9,27 @@ import java.awt.event.*;
 
 public class CreatePresenterForm extends DialogWrapper {
   private JPanel contentPane;
+  private JTextField name;
+  private JTextField packageName;
+  private JRadioButton btnNestedPresenter;
+  private JRadioButton btnPresenterWidget;
+  private JRadioButton btnPopupPresenter;
+  private JPanel buttonPanel;
+  private JPanel chooseTypesPanel;
+  private JTabbedPane tabbedPane1;
+  private JRadioButton radioRevealRoot;
+  private JRadioButton radioRevealSlot;
+  private JTextField contentSlot;
+  private JButton btnSelectContentSlot;
+  private JRadioButton radioRevealRootLayout;
+  private JCheckBox isAPlabtnIsAPlace;
+  private JTextField nameToken;
+  private JCheckBox isCrawlableCheckBox;
+  private JCheckBox codeSplitCheckBox;
+  private JCheckBox addUiHandlersCheckBox;
+  private JCheckBox addOnBindCheckBox;
+  private JCheckBox useManualRevealCheckBox;
+  private JCheckBox usePrepareFromRequestCheckBox;
 
   public CreatePresenterForm(Project project) {
     super(project);
