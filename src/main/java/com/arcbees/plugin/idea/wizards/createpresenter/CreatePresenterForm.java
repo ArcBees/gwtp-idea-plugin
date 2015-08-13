1da57158b61633999cb6cfd80535406309e7461b
Jean-Christophe Lariviere <jclariviere@arcbees.com>
2015-08-13 11:21:50 -0400

    Remove unused imports

--- src/main/java/com/arcbees/plugin/idea/wizards/createpresenter/CreatePresenterForm.java
+++ src/main/java/com/arcbees/plugin/idea/wizards/createpresenter/CreatePresenterForm.java
@@ -3,8 +3,6 @@ package com.arcbees.plugin.idea.wizards.createpresenter;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
